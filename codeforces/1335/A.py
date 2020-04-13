a49840c8bec175123c6c03732ddd4ed6d86fe942
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-04-13 20:17:00 +0530

    Add solution for problem `A - Candies and Two Sisters` Link: https://codeforces.com/contest/1335/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1335/submission/76519628

--- codeforces/1335/A.py
+++ codeforces/1335/A.py
@@ -1,10 +1,6 @@
-def solve():
-
+for _ in range(int(input())):
     n=int(input())
-    if n&1:
-        n+=1
-    print(n//2-1)
-
-for _  in range(int(input())):
-
-    solve()
+    if n%2==0:
+        print((n//2)-1)
+    else:
+        print(n//2)

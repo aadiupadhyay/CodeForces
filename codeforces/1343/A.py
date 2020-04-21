88174cd2c507fc0ca8a3c2091923fca1b5c7335d
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-04-21 20:39:00 +0530

    Add solution for problem `A - Candies` Link: https://codeforces.com/contest/1343/problem/A Tags: brute force, math, *900 Ref: https://codeforces.com/contest/1343/submission/77515578

--- codeforces/1343/A.py
+++ codeforces/1343/A.py
@@ -1,21 +1,8 @@
-from sys  import stdin,stdout
-from math import ceil
-st=lambda:list(stdin.readline().strip())
-li=lambda:list(map(int,stdin.readline().split()))
-mp=lambda:map(int,stdin.readline().split())
-inp=lambda:int(stdin.readline())
-pr=lambda n: stdout.write(str(n)+"\n")
-
-mod=1000000007
-
-def solve():
-    n=inp()
-    a=3
-    c=2
-    while n%a!=0:
-        a+=pow(2,c)
-        c+=1
-    pr(n//a)
-    
-for _ in range(inp()):
-    solve()
+for _ in range(int(input())):
+    n=int(input())
+    j=2
+    x=3
+    while n%x!=0:
+        x=x+2**j
+        j=j+1
+    print(n//x)

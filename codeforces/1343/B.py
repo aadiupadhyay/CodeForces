e1d1b5314aedc690bfb646aab11cf79749534b58
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-04-21 21:20:00 +0530

    Add solution for problem `B - Balanced Array` Link: https://codeforces.com/contest/1343/problem/B Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1343/submission/77542994

--- codeforces/1343/B.py
+++ codeforces/1343/B.py
@@ -1,34 +1,25 @@
-from sys  import stdin,stdout
-from math import ceil
-st=lambda:list(stdin.readline().strip())
-li=lambda:list(map(int,stdin.readline().split()))
-mp=lambda:map(int,stdin.readline().split())
-inp=lambda:int(stdin.readline())
-pr=lambda n: stdout.write(str(n)+"\n")
-
-mod=1000000007
-
-def solve():
-    n=inp()
-    a=n//2
-    if a&1:
-        pr('NO')
+for _ in range(int(input())):
+    n=int(input())
+    l=[]
+    if n%4!=0:
+        print("NO")
     else:
-        val=10**8
-        a=[val]
-        eve=val
-        for i in range(1,n//2):
-            eve+=2*i
-            a.append(2*i)
-        odd=0
-        for i in range(1,n//2):
-            odd+=2*i -1
-            a.append(2*i -1 )
-        a.append(eve - odd)
-        print('YES')
-        print(*a)
+        for i in range(n//2):
+            l.append(2*(i+1))
+        x=sum(l)
+        z=n//2
+        j=1
+        s=0
+        k=[]
+        while s<x:
+            a=(2*j)-1
+            s=s+a
+            j=j+1
+            k.append(a)
+        print("YES")
+        q=sum(k)-x
+        k.remove(q)
+        l=l+k
+        print(*l)
             
             
-    
-for _ in range(inp()):
-    solve()

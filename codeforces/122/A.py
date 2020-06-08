fc885ab4b9a8f29b8138c85ff65a8baf2ea5f72a
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-06-09 00:20:00 +0530

    Add solution for problem `A - Lucky Division` Link: https://codeforces.com/contest/122/problem/A Tags: brute force, number theory, *1000 Ref: https://codeforces.com/contest/122/submission/83165712

---
+++ codeforces/122/A.py
@@ -0,0 +1,5 @@
+s=int(input())
+if(s%4==0 or s%7==0 or s%47==0 or s%74==0 or s%447==0 or s%474==0 or s%744==0 or s%747==0 or s%774==0 or s%777==0 or s%444==0 or s%477==0):
+    print("YES")
+else:
+    print("NO")

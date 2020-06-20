954980c3ce3714139c5c450a195e15dfdd79999b
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-06-21 02:17:00 +0530

    Add solution for problem `C - Two Teams Composing` Link: https://codeforces.com/contest/1335/problem/C Tags: binary search, greedy, implementation, sortings, *1100 Ref: https://codeforces.com/contest/1335/submission/84530087

--- codeforces/1335/C.py
+++ codeforces/1335/C.py
@@ -1,51 +1,23 @@
-# Aaditya Upadhyay
-# ..............
-# ╭━┳━╭━╭━╮╮
-# ┃┈┈┈┣▅╋▅┫┃
-# ┃┈┃┈╰━╰━━━━━━╮
-# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
-# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
-# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
-# ╲┃┈┈┈┈╭━┳━━━━╯
-# ╲┣━━━━━━┫
-# ……….
-# .……. /´¯/)………….(\¯`\
-# …………/….//……….. …\….\
-# ………/….//……………....\….\
-# …./´¯/…./´¯\……/¯ `\…..\¯`\
-# ././…/…/…./|_…|.\….\….\…\.\
-# (.(….(….(…./.)..)...(.\.).).)
-# .\…………….\/../…....\….\/…………/
-# ..\…………….. /……...\………………../
-# …..\…………… (………....)……………./
-from sys import stdin, stdout
-from collections import *
-from math import gcd, floor, ceil
-def st(): return list(stdin.readline().strip())
-
-
-def li(): return list(map(int, stdin.readline().split()))
-def mp(): return map(int, stdin.readline().split())
-def inp(): return int(stdin.readline())
-def pr(n): return stdout.write(str(n)+"\n")
-
-
-mod = 1000000007
-INF = float('inf')
-
-
 def solve():
-    n = inp()
-    l = li()
-    d = Counter(l)
-    ans = 0
-    x = len(d)
-    for i in d:
-        cur = d[i]
-        ans = max(ans, min(cur, x-1))
-        ans = max(ans, min(cur-1, x))
-    pr(ans)
 
+    n=int(input())
 
-for _ in range(inp()):
+    l=list(map(int,input().split()))
+
+    d={}
+
+    diff=len(set(l))
+
+    for i in l:
+
+        d[i]=d.get(i,0)+1
+
+    x=list(d.values())
+
+    maxcnt=max(x)
+
+    print(max(min(diff-1,maxcnt),min(diff,maxcnt-1)))
+
+for _  in range(int(input())):
+
     solve()

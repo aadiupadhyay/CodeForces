97ad0f2d252bfadbbbe555d19b4dbba72bab2c38
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-06-20 22:12:00 +0530

    Add solution for problem `B - GCD Compression` Link: https://codeforces.com/contest/1370/problem/B Tags: constructive algorithms, math, number theory, *1100 Ref: https://codeforces.com/contest/1370/submission/84508087

--- codeforces/1370/B.py
+++ codeforces/1370/B.py
@@ -1,54 +1,46 @@
+from collections import defaultdict
 def solve():
     n=int(input())
     l=list(map(int,input().split()))
-    odd={}
-    even={}
-    oddc,evenc=0,0
+    o,e,r,lst=[],[],[],[]
     for i in range(len(l)):
- 
         if l[i]&1:
-            if oddc==0:
-                odd[i+1]=0
-                oddc=1
-            else:
-                odd[i+1]=l[i]
+            o.append(i)
         else:
-            if evenc==0:
-                even[i+1]=0
-                evenc=1
-            else:
-                even[i+1]=l[i]
- 
-    
-    x=list(odd.keys())
-    y=list(even.keys())
-    
- 
-    a,b=len(x),len(y)
- 
-    if a&1:
-        x=x[1:]
-        y=y[1:]
+            e.append(i)
+    if len(o)&1:
+        r.extend([o[0],e[0]])
     else:
-        if len(x)>0:
-            x=x[2:]
+        if len(e):
+            r.extend([e[0],e[1]])
         else:
-            y=y[2:]
- 
- 
-    for i in range(0,len(y)-1,2):
-        f,g=y[i],y[i+1]
-        print(min(f,g),max(f,g))
+            r.extend([o[0],o[1]])
+
+    s=defaultdict(lambda:False)
+
+    for i in range(2*n):
+
+        if i==r[0] or i==r[1]:
+            continue
+        if s[i]:
+            continue
+        for j in range(i+1,2*n):
+            if j==r[0] or j==r[1]:
+                continue
+            if s[j]:
+                continue
+            if (l[i]&1 and l[j]&1) or (l[i]%2==0 and l[j]%2==0):
+                s[i]=True
+                s[j]=True
+
+                lst.append((i+1,j+1))
+                break
+    for i,j in lst:
+        print(i,j)
+
         
- 
-    for j in range(0,len(x)-1,2):
-        f,g=x[j],x[j+1]
-        print(min(f,g),max(f,g))
-        
- 
-        
- 
+
     
 for _ in range(int(input())):
- 
+
     solve()

5a9f3744fd7fbc359749914ecbdd4a7225dff054
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-06-26 02:53:00 +0530

    Add solution for problem `A - QAQ` Link: https://codeforces.com/contest/894/problem/A Tags: brute force, dp, *800 Ref: https://codeforces.com/contest/894/submission/85075025

---
+++ codeforces/894/A.py
@@ -0,0 +1,10 @@
+s=input()
+n=len(s)
+c=0
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            if s[i]=='Q' and s[j]=='A' and s[k]=='Q':
+                c+=1
+print(c)
+    

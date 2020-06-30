294df03dc2c2978f86c795f70125da4029b3fae8
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-06-30 14:55:00 +0530

    Add solution for problem `D - Zero Remainder Array` Link: https://codeforces.com/contest/1374/problem/D Tags: math, sortings, two pointers, *1400 Ref: https://codeforces.com/contest/1374/submission/85524890

---
+++ codeforces/1374/D.py
@@ -0,0 +1,89 @@
+from sys  import stdin,stdout
+
+import bisect
+
+def st():
+    return list(stdin.readline())
+
+def inp():
+    return int(stdin.readline())
+
+def li():
+    return list(map(int,stdin.readline().split()))
+
+def mp():
+    return map(int,stdin.readline().split())
+
+def pr(n):
+    stdout.write(str(n)+"\n")
+
+def soe(limit):
+    l=[1]*(limit+1)
+    prime=[]
+    for i in range(2,limit+1):
+        if l[i]:
+            for j in range(i*i,limit+1,i):
+                l[j]=0
+
+    for i in range(2,limit+1):
+        if l[i]:
+            prime.append(i)
+    return prime
+
+def segsoe(low,high):
+    limit=int(high**0.5)+1
+    prime=soe(limit)
+    n=high-low+1
+    l=[0]*(n+1)
+    for i in range(len(prime)):
+        lowlimit=(low//prime[i])*prime[i]
+        if lowlimit<low:
+            lowlimit+=prime[i]
+        if lowlimit==prime[i]:
+            lowlimit+=prime[i]
+        for j in range(lowlimit,high+1,prime[i]):
+            l[j-low]=1
+    for i in range(low,high+1):
+        if not l[i-low]:
+            if i!=1:
+                print(i)
+
+def power(a,n):
+    r=1
+    while n:
+        if n&1:
+            r=(r*a)
+            n=n-1
+        else:
+            a=(a*a)
+            n=n>>1
+    return r
+    
+def solve():
+
+    n,k=mp()
+    l=li()
+    d={}
+    x=[]
+    for i in l:
+        a=i%k
+        if a!=0:
+            if a not in d:
+                d[a]=1
+                x.append(k-a)
+            else:
+                x.append((k-a)+k*d[a])
+                d[a]+=1
+    if len(x)>0:
+        pr(max(x)+1)
+    else:
+        pr(0)
+
+
+
+
+
+
+for _ in range(inp()):
+    solve()
+    

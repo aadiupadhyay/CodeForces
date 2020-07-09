6854cd32776b69b7e699210f9e32bf67aa7f85dc
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-07-10 02:16:00 +0530

    Add solution for problem `A - Display The Number` Link: https://codeforces.com/contest/1295/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/1295/submission/86382110

---
+++ codeforces/1295/A.py
@@ -0,0 +1,10 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    if(n % 2 == 1):
+        print(7, end='')
+        n -= 3
+    while(n > 0):
+        print(1, end='')
+        n -= 2
+    print()

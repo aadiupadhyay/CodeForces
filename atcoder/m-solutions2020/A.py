2a9373679327e37f529c606b19574c2ae8b0de31
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-07-25 17:32:00 +0530

    Add solution for problem `A - Kyu in AtCoder` Link: https://atcoder.jp/contests/m-solutions2020/tasks/m_solutions2020_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/m-solutions2020/submissions/15413870

---
+++ atcoder/m-solutions2020/A.py
@@ -0,0 +1,17 @@
+n=int(input())
+if n in range(400,600):
+    print(8)
+elif n in range(600,800):
+    print(7)
+elif n in range(800,1000):
+    print(6)
+elif n in range(1000,1200):
+    print(5)
+elif n in range(1200,1400):
+    print(4)
+elif n in range(1400,1600):
+    print(3)
+elif n in range(1600,1800):
+    print(2)
+else:
+    print(1)

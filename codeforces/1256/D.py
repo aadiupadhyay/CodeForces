850e42bb492757456209e4565061e88bb1beb3ba
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-07-28 18:07:00 +0530

    Add solution for problem `D - Binary String Minimizing` Link: https://codeforces.com/contest/1256/problem/D Tags: greedy, *1500 Ref: https://codeforces.com/contest/1256/submission/88203813

---
+++ codeforces/1256/D.py
@@ -0,0 +1,108 @@
+from sys  import stdin,stdout
+
+import bisect
+
+import math
+
+def st():
+    return list(stdin.readline().strip())
+
+def inp():
+    return int(stdin.readline())
+
+def li():
+    return list(map(int,stdin.readline().split()))
+
+def mp():
+    return map(int,stdin.readline().split())
+
+def pr(n):
+    stdout.write(str(n)+"\n")
+
+def soe(limit):
+    l=[1]*(limit+1)
+    prime=[]
+    for i in range(2,limit+1):
+        if l[i]:
+            for j in range(i*i,limit+1,i):
+                l[j]=0
+
+    for i in range(2,limit+1):
+        if l[i]:
+            prime.append(i)
+    return prime
+
+def segsoe(low,high):
+    limit=int(high**0.5)+1
+    prime=soe(limit)
+    n=high-low+1
+    l=[0]*(n+1)
+    for i in range(len(prime)):
+        lowlimit=(low//prime[i])*prime[i]
+        if lowlimit<low:
+            lowlimit+=prime[i]
+        if lowlimit==prime[i]:
+            lowlimit+=prime[i]
+        for j in range(lowlimit,high+1,prime[i]):
+            l[j-low]=1
+    for i in range(low,high+1):
+        if not l[i-low]:
+            if i!=1:
+                print(i)
+                
+def gcd(a,b):
+    while b:
+        a=a%b
+        b,a=a,b
+    return a
+
+def power(a,n):
+    r=1
+    while n:
+        if n&1:
+            r=(r*a)
+        a*=a
+        n=n>>1
+    return r
+    
+def solve():
+    n,k=mp()
+    s=input()
+    if '1' in s:
+        a=s.index('1')
+        if a==n-1:
+            print(s)
+            return 
+    else:
+        print(s)
+        return
+    l=['0']*a +['1']*(n-a)
+    
+    x=[i for i in range(a+1,n) if s[i]=='0']
+    
+    p=a
+    for i in range(len(x)):
+        if k==0:
+            break
+        if x[i]<=k+p:
+            k=k-x[i]+p
+            l[p]='0'
+            x[i]=0
+            p+=1
+        else:
+            l[x[i]-k]='0'
+            x[i]=0
+            k=0
+        
+    for i in x:
+        if i!=0:
+            l[i]='0'
+    print(''.join(l))
+    
+            
+            
+
+
+for _ in range(inp()):
+    solve()
+    

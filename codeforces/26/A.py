7ae3e7def03ed087c776783687176062af151a68
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-08-01 22:57:00 +0530

    Add solution for problem `A - Almost Prime` Link: https://codeforces.com/contest/26/problem/A Tags: number theory, *900 Ref: https://codeforces.com/contest/26/submission/88684128

---
+++ codeforces/26/A.py
@@ -0,0 +1,86 @@
+from sys  import stdin,stdout
+
+import bisect
+
+import math
+
+def st():
+    return list(stdin.readline().strip())
+
+def inp():
+    return int(stdin.readline())
+
+def li():
+    return list(map(int,stdin.readline().split()))
+
+def mp():
+    return map(int,stdin.readline().split())
+
+def pr(n):
+    stdout.write(str(n)+"\n")
+
+def soe(limit):
+    l=[1]*(limit+1)
+    prime=[2]
+    for i in range(2,limit+1):
+        if l[i]:
+            for j in range(i*i,limit+1,i):
+                l[j]=0
+    
+    for i in range(3,limit+1,2):
+        if l[i]:
+            prime.append(i)
+    return prime
+
+def segsoe(low,high):
+    limit=int(high**0.5)+1
+    prime=soe(limit)
+    n=high-low+1
+    l=[0]*(n+1)
+    for i in range(len(prime)):
+        lowlimit=(low//prime[i])*prime[i]
+        if lowlimit<low:
+            lowlimit+=prime[i]
+        if lowlimit==prime[i]:
+            lowlimit+=prime[i]
+        for j in range(lowlimit,high+1,prime[i]):
+            l[j-low]=1
+    for i in range(low,high+1):
+        if not l[i-low]:
+            if i!=1:
+                print(i)
+                
+def gcd(a,b):
+    while b:
+        a=a%b
+        b,a=a,b
+    return a
+
+def power(a,n):
+    r=1
+    while n:
+        if n&1:
+            r=(r*a)
+        a*=a
+        n=n>>1
+    return r
+    
+def solve():
+    x=soe(3010)
+    y=[0]*(3010)
+    for i in x:
+        for j in range(2*i,3010,i):
+            y[j]+=1
+    c=0
+    for i in range(inp()+1):
+        if y[i]==2:
+            c+=1
+    pr(c)
+        
+    
+
+
+
+for _ in range(1):
+    solve()
+    

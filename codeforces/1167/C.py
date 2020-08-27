1bba0e1c7c72586168c0d1eb4090f74f6a0e5c79
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-08-27 14:59:00 +0530

    Add solution for problem `C - News Distribution` Link: https://codeforces.com/contest/1167/problem/C Tags: dfs and similar, dsu, graphs, *1400 Ref: https://codeforces.com/contest/1167/submission/91120296

--- codeforces/1167/C.py
+++ codeforces/1167/C.py
@@ -1,16 +1,39 @@
 import sys,bisect
- 
+
 from sys  import stdin,stdout
- 
- 
+
+from bisect import bisect_left,bisect_right,bisect,insort,insort_left,insort_right
+
+from math import gcd,ceil,floor,sqrt
+
+from collections import Counter,defaultdict,deque,OrderedDict
+
+from queue import Queue,PriorityQueue
+
+from string import ascii_lowercase
+
+from heapq import *
+
+from itertools import islice
+
+
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+MOD = 998244353
+mod = 10**9+7
+
+def st():
+    return list(stdin.readline().strip())
+
+def inp():
+    return int(stdin.readline())
+
 def li():
     return list(map(int,stdin.readline().split()))
- 
+
 def mp():
     return map(int,stdin.readline().split())
- 
 
- 
 def solve():
     n,m=mp()
     d={i:[] for i in range(1,n+1)}
@@ -21,7 +44,7 @@ def solve():
             for i in range(1,x):
                 d[l[i]].append(l[i+1])
                 d[l[i+1]].append(l[i])
- 
+
     
                 
     ans=[-1 for i in range(n+1)]
@@ -41,10 +64,11 @@ def solve():
                         vi[x]=i
                         stack.append(x)
                     
- 
+
     print(' '.join((str(ans[vi[i]]) for i in range(1,n+1))))
     
- 
+
 for _ in range(1):
     solve()
- 
+##    print("Case #{}:".format(_+1),c)
+##    

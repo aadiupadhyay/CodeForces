be626585668c0ff609cf2e69f663f8cd5c52f497
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-08-27 15:25:00 +0530

    Add solution for problem `C - News Distribution` Link: https://codeforces.com/contest/1167/problem/C Tags: dfs and similar, dsu, graphs, *1400 Ref: https://codeforces.com/contest/1167/submission/91121994

--- codeforces/1167/C.py
+++ codeforces/1167/C.py
@@ -1,11 +1,15 @@
-from sys  import stdin,stdout
 
-st=lambda:list(stdin.readline().strip())
-li=lambda:list(map(int,stdin.readline().split()))
-mp=lambda:map(int,stdin.readline().split())
-inp=lambda:int(stdin.readline())
-pr=lambda n: stdout.write(str(n)+"\n")
-
+from sys  import stdin
+ 
+ 
+def li():
+    return list(map(int,stdin.readline().split()))
+ 
+def mp():
+    return map(int,stdin.readline().split())
+ 
+ 
+ 
 def solve():
     n,m=mp()
     d={i:[] for i in range(1,n+1)}

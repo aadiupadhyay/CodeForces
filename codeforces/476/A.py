600ef709a2f71b49781fbb0cd9def6f5f9dd3c1e
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-09-03 01:26:00 +0530

    Add solution for problem `A - Dreamoon and Stairs` Link: https://codeforces.com/contest/476/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/476/submission/91671963

--- codeforces/476/A.py
+++ codeforces/476/A.py
@@ -1,24 +1,13 @@
-from sys  import stdin,stdout
-
-li=lambda:list(map(int,stdin.readline().split()))
-mp=lambda:map(int,stdin.readline().split())
-inp=lambda:int(stdin.readline())
-pr=lambda n: stdout.write(str(n)+"\n")
-
-def solve():
-    n,m=mp()
-    a=n>>1
-    c=0
-    b=n%2
-    for i in range(a+1):
-        if (a+b)%m==0:
-            pr(a+b)
-            return 
-        a-=1
-        b+=2
-    pr(-1)
- 
- 
- 
-for _ in range(1):
-    solve()
+n,m=map(int,input().split())
+a=n>>1
+c=0
+b=n%2
+for i in range(a+1):
+    if (a+b)%m==0:
+        print(a+b)
+        c=1
+        break
+    a-=1
+    b+=2
+if c==0:
+    print(-1)

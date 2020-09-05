92d1f748fafa85abc8eaa00817691206ebb17e64
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-09-05 14:37:00 +0530

    Add solution for problem `C - Yet Another Array Restoration` Link: https://codeforces.com/contest/1409/problem/C Tags: brute force, math, number theory, *1200 Ref: https://codeforces.com/contest/1409/submission/91924861

--- codeforces/1409/C.py
+++ codeforces/1409/C.py
@@ -1,66 +1,16 @@
-# Aaditya Upadhyay
-# ..............
-# ╭━┳━╭━╭━╮╮
-# ┃┈┈┈┣▅╋▅┫┃
-# ┃┈┃┈╰━╰━━━━━━╮
-# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
-# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
-# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
-# ╲┃┈┈┈┈╭━┳━━━━╯
-# ╲┣━━━━━━┫
-# ……….
-# .……. /´¯/)………….(\¯`\
-# …………/….//……….. …\….\
-# ………/….//……………....\….\
-# …./´¯/…./´¯\……/¯ `\…..\¯`\
-# ././…/…/…./|_…|.\….\….\…\.\
-# (.(….(….(…./.)..)...(.\.).).)
-# .\…………….\/../…....\….\/…………/
-# ..\…………….. /……...\………………../
-# …..\…………… (………....)……………./
-from sys import stdin, stdout
-from collections import *
-from math import gcd, floor, ceil
-def st(): return list(stdin.readline().strip())
-
-
-def li(): return list(map(int, stdin.readline().split()))
-def mp(): return map(int, stdin.readline().split())
-def inp(): return int(stdin.readline())
-def pr(n): return stdout.write(str(n)+"\n")
-
-
-mod = 1000000007
-INF = float('inf')
-
 
 def solve():
-    n, x, y = mp()
-    val = y-x
-    cur = n-1
-    ans = [x, y]
-    while val % cur:
-        cur -= 1
-    cur = val//cur
-    maxi = y
-    more = n-2
-    copy = x
-    x -= cur
-    while more and copy+cur != y:
-        more -= 1
-        copy += cur
-        ans.append(copy)
-    while more and x > 0:
-        more -= 1
-        ans.append(x)
-        x -= cur
-    new = y+cur
-    while more:
-        ans.append(new)
-        new += cur
-        more -= 1
+    n,x,y=map(int,input().split())
+    maxx=float('inf')
+    for i in range(1,51):
+        for j in range(1,51):
+            l=[i+ k*j for k in range(n)]
+            if (x in l) and (y in l) and l[-1]<maxx:
+                maxx=l[-1]
+                ans=l
     print(*ans)
+        
 
 
-for _ in range(inp()):
+for _ in range(int(input())):
     solve()

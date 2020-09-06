6884db4fe33987219c42de8cfdbcf30e34eb75f4
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-09-06 20:50:00 +0530

    Add solution for problem `B - Array Cancellation` Link: https://codeforces.com/contest/1405/problem/B Tags: constructive algorithms, implementation, *1000 Ref: https://codeforces.com/contest/1405/submission/92050661

---
+++ codeforces/1405/B.py
@@ -0,0 +1,39 @@
+import sys,bisect
+
+from sys  import stdin,stdout
+
+from bisect import bisect_left,bisect_right,bisect,insort,insort_left,insort_right
+
+from math import gcd,ceil,floor,sqrt
+
+
+def st():
+    return list(stdin.readline().strip())
+
+def inp():
+    return int(stdin.readline())
+
+def li():
+    return list(map(int,stdin.readline().split()))
+
+def mp():
+    return map(int,stdin.readline().split())
+
+def pr(n):
+    stdout.write(str(n)+"\n")
+
+    
+def solve():
+    n=inp()
+    l=li()
+    x=[-i for i in l]
+    y=[0]
+    for i in x:
+        y.append(y[-1]+i)
+    z=[i for i in y if i>=0]
+    print(max(z))
+
+
+for _ in range(inp()):
+    solve()
+  

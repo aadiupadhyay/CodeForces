c0b81256d66617af60c98d5e668c89ed251fc69e
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-09-21 02:24:00 +0530

    Add solution for problem `D - Divide by three, multiply by two` Link: https://codeforces.com/contest/977/problem/D Tags: dfs and similar, math, sortings, *1400 Ref: https://codeforces.com/contest/977/submission/93366433

--- codeforces/977/D.py
+++ codeforces/977/D.py
@@ -1,17 +1,16 @@
 from sys  import stdin,stdout
 from collections import deque
- 
+
 st=lambda:list(stdin.readline().strip())
 li=lambda:list(map(int,stdin.readline().split()))
 mp=lambda:map(int,stdin.readline().split())
 inp=lambda:int(stdin.readline())
 pr=lambda n: stdout.write(str(n)+"\n")
- 
+
 mod=1000000007
- 
+
 def solve():
     n=inp()
-    f=0
     l=li()
     d={i:[] for i in l}
     m=max(d)
@@ -25,16 +24,17 @@ def solve():
    
     for i in l:
         q=deque()
+        v=set()
         par={i:-1 for i in l}
         q.append(i)
         ans=[i]
-        
+        v.add(i)
         while q:
             a=q.popleft()
             for i in d[a]:
                 par[i]=a
                 q.append(i)
- 
+
         if list(par.values()).count(-1)==1:
             dd={}
             for i in par:
@@ -45,10 +45,6 @@ def solve():
             for i in range(n-1):
                 print(dd[x],end=' ' )
                 x=dd[x]
-            
-            f=1
-        if f:
-            break
                 
             
             
@@ -56,6 +52,6 @@ def solve():
         
         
             
- 
+
 for _ in range(1):
     solve()

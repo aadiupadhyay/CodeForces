9894a4f2e54c6f4694b24e8e6eab32bba61bb0d9
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-09-21 14:41:00 +0530

    Add solution for problem `D - Divide by three, multiply by two` Link: https://codeforces.com/contest/977/problem/D Tags: dfs and similar, math, sortings, *1400 Ref: https://codeforces.com/contest/977/submission/93390957

---
+++ codeforces/977/D.py
@@ -0,0 +1,61 @@
+from sys  import stdin,stdout
+from collections import deque
+ 
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+ 
+mod=1000000007
+ 
+def solve():
+    n=inp()
+    f=0
+    l=li()
+    d={i:[] for i in l}
+    m=max(d)
+    for i in range(n):
+        if 2*l[i] <= m:
+            if 2*l[i] in l:
+                d[l[i]].append(2*l[i])
+        if l[i]%3==0:
+            if l[i]//3 in l:
+                d[l[i]].append(l[i]//3)
+   
+    for i in l:
+        q=deque()
+        par={i:-1 for i in l}
+        q.append(i)
+        ans=[i]
+        
+        while q:
+            a=q.popleft()
+            for i in d[a]:
+                par[i]=a
+                q.append(i)
+ 
+        if list(par.values()).count(-1)==1:
+            dd={}
+            for i in par:
+                dd[par[i]]=i
+            
+            x=dd[-1]
+            print(x,end=' ')
+            for i in range(n-1):
+                print(dd[x],end=' ' )
+                x=dd[x]
+            
+            f=1
+        if f:
+            break
+                
+            
+            
+        
+        
+        
+            
+ 
+for _ in range(1):
+    solve()

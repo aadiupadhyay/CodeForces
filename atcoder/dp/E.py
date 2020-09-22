871ed8a3884ce63cafa78c4aca9bf9acda568640
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-09-22 23:23:00 +0530

    Add solution for problem `E - Knapsack 2` Link: https://atcoder.jp/contests/dp/tasks/dp_e Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16962329

--- atcoder/dp/E.py
+++ atcoder/dp/E.py
@@ -1,36 +1,15 @@
-from sys  import stdin,stdout
-from math import gcd
-st=lambda:list(stdin.readline().strip())
-li=lambda:list(map(int,stdin.readline().split()))
-mp=lambda:map(int,stdin.readline().split())
-inp=lambda:int(stdin.readline())
-pr=lambda n: stdout.write(str(n)+"\n")
-
-mod=1000000007
-
-def solve():
-    n,k=mp()
-    knap=[li() for i in range(n)]
-    s=sum([knap[i][1] for i in range(n)])
-    dp=[[float('inf') for i in range(s+1)] for j in range(n+1)]
-    dp[0][0]=0
-    for i in range(1,n+1):
-        a,b=knap[i-1]
-        for j in range(s+1):
-            if j==0:
-                dp[i][j]=0
-                
-            elif j>=b:
-                
-                dp[i][j]=min(dp[i-1][j],dp[i-1][j-b]+a)
-            else:
-                dp[i][j]=dp[i-1][j]
-
-    while dp[n][s]>k:
-        s-=1
-    pr(s)
-        
-                
-        
-for _ in range(1):
-    solve()
+INF=10**18
+n,m=map(int,input().split())
+w=[0]*n
+v=[0]*n
+for i in range(n):
+  w[i],v[i]=map(int,input().split())
+k=sum(v)
+dp=[INF]*(k+1)
+dp[0]=0
+for i in range(n):
+  for j in range(k,-1,-1):
+    if j>=v[i] and dp[j-v[i]]!=INF:dp[j]=min(dp[j],dp[j-v[i]]+w[i])
+for i,x in enumerate(dp):
+  if x<=m:ans=i
+print(ans)

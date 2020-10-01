8545e0c11730e12f883611c0b3272bf2e88578fb
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-10-02 03:28:00 +0530

    Add solution for problem `D - I hate Factorization` Link: https://atcoder.jp/contests/abc166/tasks/abc166_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc166/submissions/17132380

--- atcoder/abc166/D.py
+++ atcoder/abc166/D.py
@@ -8,8 +8,8 @@ pr=lambda n: stdout.write(str(n)+"\n")
 
 def solve():
     n=inp()
-    for i in range(-400,400):
-        for j in  range(-400,400):
+    for i in range(-500,500):
+        for j in  range(-500,500):
             if pow(i,5) - pow(j,5) == n:
                 print(i,j)
                 return

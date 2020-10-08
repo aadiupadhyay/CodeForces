91c5433ba6c1fd568ed94cc82ad89f59f6041015
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-10-09 00:34:00 +0530

    Add solution for problem `C - Yet Another Walking Robot` Link: https://codeforces.com/contest/1296/problem/C Tags: data structures, implementation, *1500 Ref: https://codeforces.com/contest/1296/submission/94940664

--- codeforces/1296/C.py
+++ codeforces/1296/C.py
@@ -1,61 +1,30 @@
-from sys  import stdin,stdout
-from collections import deque
-st=lambda:list(stdin.readline().strip())
-li=lambda:list(map(int,stdin.readline().split()))
-mp=lambda:map(int,stdin.readline().split())
-inp=lambda:int(stdin.readline())
-pr=lambda n: stdout.write(str(n)+"\n")
-
-mod=1000000007
-INF=float('inf')
-
-
-def solve():
-    MIN=INF
-    d={(0,0):0}
-    n=inp()
-    s=['Z']+st()
-    x,y=0,0
-    X_cor,Y_cor=-1,-1
-    for i in range(1,n+1):
-        if s[i]=='L':
-            x-=1
-        if s[i]=='R':
-            x+=1
-        if s[i]=='D':
-            y-=1
-        if s[i]=='U':
-            y+=1
-        
-        if (x,y) in d:
-
-            A= d[(x,y)]
-            if i-A< MIN:
-                X_cor=A+1
-                Y_cor=i
-                MIN= i-A
-                d[(x,y)]=i
-            else:
-                d[(x,y)]=i
-                
-        else:
-            d[(x,y)]=i
-
-     
-    if MIN!=INF:
-        print(X_cor,Y_cor)
-    else:
+INF = float('inf')
+ 
+for i in range(int(input())):
+    n = int(input())
+    s = input()
+ 
+    x, y = (0, 0)
+    d = {(0, 0): 0}
+ 
+    m = INF
+    mi = -1
+ 
+    for i in range(n):
+        if s[i] == 'L': x -= 1
+        if s[i] == 'R': x += 1
+ 
+        if s[i] == 'U': y += 1
+        if s[i] == 'D': y -= 1
+ 
+        if (x, y) in d:
+            if m > (i + 1) - d[x, y]:
+                m = (i + 1) - d[x, y]
+                mi = i + 1
+ 
+        d[x, y] = i + 1
+ 
+    if mi == -1:
         print(-1)
-                
-                
-    
-
-for _ in range(inp()):
-
-    solve()
-    
-    
-    
-                
-        
-
+    else:
+        print(mi - m + 1, mi)

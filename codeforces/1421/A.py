eb719406afbe99e31f51dddb007e913d021fa044
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-10-18 14:43:00 +0530

    Add solution for problem `A - XORwice` Link: https://codeforces.com/contest/1421/problem/A Tags: bitmasks, greedy, math, *800 Ref: https://codeforces.com/contest/1421/submission/95861363

---
+++ codeforces/1421/A.py
@@ -0,0 +1,19 @@
+    
+from sys  import stdin,stdout
+
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+
+def solve():
+    a,b= mp()
+    print(a^b)
+        
+    
+    
+for _ in range(inp()):
+    solve()

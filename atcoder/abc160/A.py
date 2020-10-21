cc506925796f1969e7681b3e3a40b580b7162f4b
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-10-21 23:32:00 +0530

    Add solution for problem `A - Coffee` Link: https://atcoder.jp/contests/abc160/tasks/abc160_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc160/submissions/17563779

---
+++ atcoder/abc160/A.py
@@ -0,0 +1,22 @@
+from sys  import stdin,stdout
+
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+
+def solve():
+    s=st()
+    if s[2]==s[3] and s[4]==s[5]:
+        pr('Yes')
+    else:
+        pr('No')
+    
+                
+        
+    
+for _ in range(1):
+    solve()

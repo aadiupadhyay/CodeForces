993f334cc1ca0fb453baed7c97de5b08609ba713
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-11-22 23:49:00 +0530

    Add solution for problem `A - Determinant` Link: https://atcoder.jp/contests/abc184/tasks/abc184_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc184/submissions/18355500

---
+++ atcoder/abc184/A.py
@@ -0,0 +1,21 @@
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    a,b = mp()
+    c,d = mp()
+    pr(a*d-b*c)
+
+        
+            
+    
+solve()

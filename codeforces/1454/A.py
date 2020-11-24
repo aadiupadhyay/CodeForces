2e30f7aff350cdc1316af2c6eceb7f2630eae988
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-11-24 20:09:00 +0530

    Add solution for problem `A - Special Permutation` Link: https://codeforces.com/contest/1454/problem/A Tags: constructive algorithms, probabilities, *800 Ref: https://codeforces.com/contest/1454/submission/99413125

---
+++ codeforces/1454/A.py
@@ -0,0 +1,22 @@
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n=inp()
+    for i in range(n-1):
+        print(i+2,end=' ')
+    print(1)
+        
+            
+    
+for _ in range(inp()):
+    solve()

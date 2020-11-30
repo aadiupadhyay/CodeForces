63f45c344e22abb662fb628e7886fbc6dbab6056
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-11-30 22:19:00 +0530

    Add solution for problem `C - Ping-pong` Link: https://codeforces.com/contest/1455/problem/C Tags: constructive algorithms, games, math, *1100 Ref: https://codeforces.com/contest/1455/submission/100054537

---
+++ codeforces/1455/C.py
@@ -0,0 +1,20 @@
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    x,y = mp()
+    print(x-1,y)
+        
+            
+    
+for _ in range(inp()):
+    solve()

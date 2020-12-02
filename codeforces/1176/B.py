9fa3859b35340fd592e8bd6ced440804d05a0870
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-02 14:59:00 +0530

    Add solution for problem `B - Merge it!` Link: https://codeforces.com/contest/1176/problem/B Tags: math, *1100 Ref: https://codeforces.com/contest/1176/submission/100174682

---
+++ codeforces/1176/B.py
@@ -0,0 +1,52 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n=inp()
+    l=li()
+    ans=0
+    a,b =0,0
+    for i in l:
+        ans+= (i%3==0)
+        a+= (i%3==1)
+        b+= (i%3==2)
+    x= min(a,b)
+    ans+=x 
+    a-=x
+    b-=x
+    ans+= a//3
+    ans+= b//3
+    pr(ans)
+     
+    
+for _ in range(inp()):
+    solve()

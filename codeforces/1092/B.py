314d895768c8aaf2ea3af0bbc25d9508cb3e74df
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-03 22:41:00 +0530

    Add solution for problem `B - Teams Forming` Link: https://codeforces.com/contest/1092/problem/B Tags: sortings, *800 Ref: https://codeforces.com/contest/1092/submission/100293693

---
+++ codeforces/1092/B.py
@@ -0,0 +1,43 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n=inp()
+    l=li()
+    ans=0
+    l.sort()
+    for i in range(0,n-1,2):
+        ans+= abs(l[i]-l[i+1])
+    pr(ans)
+    
+for _ in range(1):
+    solve()

9c66d9da76da56dfc1910edf83738b28bde87597
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-03 23:47:00 +0530

    Add solution for problem `C - Prefix Sum Primes` Link: https://codeforces.com/contest/1150/problem/C Tags: constructive algorithms, greedy, math, number theory, *1200 Ref: https://codeforces.com/contest/1150/submission/100298119

---
+++ codeforces/1150/C.py
@@ -0,0 +1,55 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n=inp()
+    l=li()
+    d=Counter(l)
+    ans=[]
+    if 1 in d and 2 in d:
+        ans.append(2)
+        ans.append(1)
+        d[2]-=1
+        d[1]-=1
+        ans+=[2]*d[2]
+        ans+=[1]*d[1]
+    else:
+        if d[1]:
+            ans=[1]*d[1]
+        else:
+            ans=[2]*d[2]
+
+    print(*ans)
+     
+    
+for _ in range(1):
+    solve()

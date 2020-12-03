556e3815c3319f0ecec71903f2183f74386f444a
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-04 01:11:00 +0530

    Add solution for problem `B1 - Social Network (easy version)` Link: https://codeforces.com/contest/1234/problem/B1 Tags: implementation, *1000 Ref: https://codeforces.com/contest/1234/submission/100302676

---
+++ codeforces/1234/B1.py
@@ -0,0 +1,50 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n,m = mp()
+    l=li()
+    q=deque()
+    for i in l:
+        if i in q:
+            continue 
+        if len(q)<m:
+            q.appendleft(i)
+        else:
+            q.pop()
+            q.appendleft(i)
+    print(len(q))
+    print(*q)
+
+    
+for _ in range(1):
+    solve()

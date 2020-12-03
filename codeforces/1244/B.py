a22482aa0fe05f5d89d025724eef004bb3c3dba6
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-04 01:01:00 +0530

    Add solution for problem `B - Rooms and Staircases` Link: https://codeforces.com/contest/1244/problem/B Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/1244/submission/100302176

---
+++ codeforces/1244/B.py
@@ -0,0 +1,46 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n=inp()
+    s=st()
+    i=0
+    ans=n
+    while i<n :
+        if s[i]=='1':
+            ans=max(ans,max(2*i+2,2*(n-i)))
+        i+=1
+    pr(ans)
+    
+    
+for _ in range(inp()):
+    solve()

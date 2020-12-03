6d72e34cb2673424621bb0d39ca5778e2453b3e5
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-03 22:48:00 +0530

    Add solution for problem `B - Keyboard Layouts` Link: https://codeforces.com/contest/831/problem/B Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/831/submission/100294130

---
+++ codeforces/831/B.py
@@ -0,0 +1,54 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    a=st()
+    b=st()
+    s=st()
+    d={a[i]:i for i in range(26)}
+    dd={i:b[i] for i in range(26)}
+    ans=''
+    for i in s:
+        if i.isdigit():
+            ans+=i
+        else:
+            x=i.lower()
+            y=dd[d[x]]
+            if i.isupper():
+                ans+=y.upper()
+            else:
+                ans+=y
+    pr(ans)
+     
+    
+for _ in range(1):
+    solve()

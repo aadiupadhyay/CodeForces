5a7701ed5b3ee5da45fdc1c33d1a0215a1157e13
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-05 23:08:00 +0530

    Add solution for problem `B - Getting an A` Link: https://codeforces.com/contest/991/problem/B Tags: greedy, sortings, *900 Ref: https://codeforces.com/contest/991/submission/100472041

---
+++ codeforces/991/B.py
@@ -0,0 +1,54 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n=inp()
+    l=li()
+    s=sum(l)
+    l.sort()
+    ans=0
+    #print(16/4)
+    i=0
+    while i<n and round(s/n)!=5:
+        #print(s/n)
+        if s/n==4.5:
+            break
+        s-=l[i]
+        s+=5
+        ans+=1
+        #print(s)
+        i+=1
+    pr(ans)
+
+    
+for _ in range(1):
+    solve()

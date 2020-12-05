ad2e4a25372712fd9f81643e0f6607290ab8889e
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-06 00:38:00 +0530

    Add solution for problem `B - Duff in Love` Link: https://codeforces.com/contest/588/problem/B Tags: math, *1300 Ref: https://codeforces.com/contest/588/submission/100476248

---
+++ codeforces/588/B.py
@@ -0,0 +1,59 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    n=inp()
+    factor=[]
+    i=1
+    while i*i<=n:
+        if n%i==0:
+            factor.append(i)
+            if i!=n//i:
+                factor.append(n//i)
+        i+=1
+    factor.sort(reverse=True)
+    for i in factor:
+        j=2
+        f=0
+        while j*j<=i:
+            if i%(j*j)==0:
+                f=1
+                break
+            j+=1
+        if f==0:
+            pr(i)
+            return
+
+     
+    
+for _ in range(1):
+    solve()

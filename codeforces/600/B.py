07c088376110c5bb3dc0a24665d34be5d7ebf908
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-05 23:48:00 +0530

    Add solution for problem `B - Queries about less or equal elements` Link: https://codeforces.com/contest/600/problem/B Tags: binary search, data structures, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/600/submission/100474038

--- codeforces/600/B.py
+++ codeforces/600/B.py
@@ -32,24 +32,13 @@ mod=1000000007
 INF=float('inf')
 
 def solve():
-    def BIN(l,x):
-        left,right =0,n-1
-        while left<=right:
-            mid= (left+right)>>1
-            if l[mid]>x and (l[mid-1]<=x or mid==0):
-                return mid
-            if l[mid]<=x:
-                left=mid+1
-            else:
-                right=mid-1
-        return left
-        
     n,q = mp()
     l=li()
     k=li()
     l.sort()
-    ans=[BIN(l,i) for i in k]
+    ans=[bisect_right(l,i) for i in k]
     print(*ans)
+     
     
 for _ in range(1):
     solve()

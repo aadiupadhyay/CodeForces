f0eba978c7e9d5b4a9ac598d3cc7ff185513d7da
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-06 00:22:00 +0530

    Add solution for problem `B - Queries about less or equal elements` Link: https://codeforces.com/contest/600/problem/B Tags: binary search, data structures, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/600/submission/100475545

---
+++ codeforces/600/B.py
@@ -0,0 +1,55 @@
+# Aaditya Upadhyay 
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./     
+from sys  import stdin,stdout
+from collections  import *
+from math import gcd,floor,ceil
+from bisect import bisect_right
+st=lambda:list(stdin.readline().strip())
+li=lambda:list(map(int,stdin.readline().split()))
+mp=lambda:map(int,stdin.readline().split())
+inp=lambda:int(stdin.readline())
+pr=lambda n: stdout.write(str(n)+"\n")
+
+mod=1000000007
+INF=float('inf')
+
+def solve():
+    def BIN(l,x):
+        left,right =0,n-1
+        while left<=right:
+            mid= (left+right)>>1
+            if l[mid]>x and (l[mid-1]<=x or mid==0):
+                return mid
+            if l[mid]<=x:
+                left=mid+1
+            else:
+                right=mid-1
+        return left
+        
+    n,q = mp()
+    l=li()
+    k=li()
+    l.sort()
+    ans=[bisect_right(l,i) for i in k]
+    print(' '.join(map(str,ans)))
+    
+for _ in range(1):
+    solve()

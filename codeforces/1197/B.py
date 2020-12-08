62a547fc6beef554e0a09274f2466324132e3d11
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-08 22:33:00 +0530

    Add solution for problem `B - Pillars` Link: https://codeforces.com/contest/1197/problem/B Tags: greedy, implementation, *1000 Ref: https://codeforces.com/contest/1197/submission/100715658

---
+++ codeforces/1197/B.py
@@ -0,0 +1,62 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    l = li()
+    x = sorted(l, reverse=True)
+    ind = -1
+    for i in range(n):
+        if l[i] == x[0]:
+            ind = i
+            break
+    a = ind-1
+    b = ind+1
+    i = 1
+    while i < n:
+        if a >= 0 and l[a] == x[i]:
+            a -= 1
+        elif b < n and l[b] == x[i]:
+            b += 1
+        else:
+            pr('NO')
+            return
+        i += 1
+    pr('YES')
+
+
+for _ in range(1):
+    solve()

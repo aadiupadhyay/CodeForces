e386a8511e84799da33c7185d9000c557bf35fd5
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-08 18:10:00 +0530

    Add solution for problem `B - Ksusha the Squirrel` Link: https://codeforces.com/contest/299/problem/B Tags: brute force, implementation, *900 Ref: https://codeforces.com/contest/299/submission/100694747

---
+++ codeforces/299/B.py
@@ -0,0 +1,56 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n, k = mp()
+    s = st()
+    i = 0
+    while i < n:
+        if s[i] == '#':
+            c = 0
+            while i < n and s[i] == '#':
+                i += 1
+                c += 1
+            if c >= k:
+                pr('NO')
+                return
+        else:
+            i += 1
+    pr('YES')
+
+
+for _ in range(1):
+    solve()

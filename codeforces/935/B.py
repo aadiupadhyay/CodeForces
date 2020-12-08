98d0268fe498f81b0e75fc7d2cccd09f7582e978
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-08 15:45:00 +0530

    Add solution for problem `B - Fafa and the Gates` Link: https://codeforces.com/contest/935/problem/B Tags: implementation, *900 Ref: https://codeforces.com/contest/935/submission/100684411

---
+++ codeforces/935/B.py
@@ -0,0 +1,58 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    ans = 0
+    s = st()
+    cur = -1
+    a, b = 0, 0
+    for i in range(n):
+        if s[i] == 'R':
+            a += 1
+        else:
+            b += 1
+        if a > b:
+            if cur != 0:
+                ans += 1
+                cur = 0
+        if b > a:
+            if cur != 1:
+                ans += 1
+                cur = 1
+    pr(ans-1)
+
+
+for _ in range(1):
+    solve()

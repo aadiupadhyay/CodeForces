228e3c1a465646c5ab29334abb3c85b39bbb5f39
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-12 01:54:00 +0530

    Add solution for problem `A - Best Subsegment` Link: https://codeforces.com/contest/1117/problem/A Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/1117/submission/100973663

---
+++ codeforces/1117/A.py
@@ -0,0 +1,56 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    l = li()
+    a = max(l)
+    i = 0
+    ans = 0
+    while i < n:
+        if l[i] == a:
+            c = 0
+            while i < n and l[i] == a:
+                c += 1
+                i += 1
+            ans = max(ans, c)
+        else:
+            i += 1
+    pr(ans)
+
+
+for _ in range(1):
+    solve()

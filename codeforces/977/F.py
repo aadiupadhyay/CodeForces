6782c89dac29f73414a5da9573d63673723af7d6
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-14 01:31:00 +0530

    Add solution for problem `F - Consecutive Subsequence` Link: https://codeforces.com/contest/977/problem/F Tags: dp, *1700 Ref: https://codeforces.com/contest/977/submission/101129527

---
+++ codeforces/977/F.py
@@ -0,0 +1,56 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    l = li()
+    d = defaultdict(int)
+    for i in l:
+        d[i] = d[i-1]+1
+    ma = max(d, key=lambda x: d[x])
+    val = d[ma]
+    ans = []
+    while n:
+        if l[n-1] == ma:
+            ans += n,
+            ma -= 1
+        n -= 1
+    pr(val)
+    print(*ans[::-1])
+
+
+for _ in range(1):
+    solve()

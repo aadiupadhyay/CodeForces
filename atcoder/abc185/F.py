b82a196a4eea57f329bfddade7dd159cab315d58
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-13 18:06:00 +0530

    Add solution for problem `F - Range Xor Query` Link: https://atcoder.jp/contests/abc185/tasks/abc185_f Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc185/submissions/18742980

--- atcoder/abc185/F.py
+++ atcoder/abc185/F.py
@@ -1,48 +1,83 @@
-def update(ind, start, end, pos, val):
-    if start == end:
-        segment[ind] = l[start]
-        return
-    mid = (start+end)//2
-    if pos <= mid:
-        update(2*ind+1, start, mid, pos, val)
-    else:
-        update(2*ind+2, mid+1, end, pos, val)
-
-    segment[ind] = segment[2*ind+1] ^ segment[2*ind+2]
-
-
-def query(ind, start, end, q1, q2):
-    if q1 > end or q2 < start:
-        return 0
-    if q1 <= start and q2 >= end:
-        return segment[ind]
-    mid = (start+end)//2
-    return query(2*ind+1, start, mid, q1, q2) ^ query(2*ind+2, mid+1, end, q1, q2)
-
-
-def build(ind, start, end):
-    if start == end:
-        segment[ind] = l[start]
-        return
-    mid = (start+end)//2
-    build(2*ind+1, start, mid)
-    build(2*ind+2, mid+1, end)
-
-    segment[ind] = segment[2*ind+1] ^ segment[2*ind+2]
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
 
 
-n, m = map(int, input().split())
-l = list(map(int, input().split()))
-segment = [0]*(4*n)
-build(0, 0, n-1)
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    b -= 1
-    if a == 1:
-        l[b] ^= c
-        update(0, 0, n-1, b, c)
 
-    else:
-        c -= 1
-        print(query(0, 0, n-1, b, c))
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    def build(n):
+        for i in range(n-1, 0, -1):
+            segment[i] = segment[2*i + 1] ^ segment[2*i]
+
+    def query(a, b, n):
+        a += n
+        b += n
+        s = 0
+        while a <= b:
+            if a & 1:
+                s ^= segment[a]
+                a += 1
+            if b % 2 == 0:
+                s ^= segment[b]
+                b -= 1
+            a = a >> 1
+            b = b >> 1
+        return s
+
+    def update(ind, val, n):
+        ind += n
+        segment[ind] ^= val
+        while ind > 1:
+            ind = ind >> 1
+            segment[ind] = segment[2*ind] ^ segment[2*ind+1]
+    n, q = mp()
+    l = li()
+    segment = [0]*(2*n)
+    for i in range(n):
+        segment[i+n] = l[i]
+    build(n)
+    ans = []
+    for i in range(q):
+        a, b, c = mp()
+        b -= 1
+        if a == 2:
+            c-=1
+            A = query(b, c, n)
+            ans.append(A)
+        else:
+            update(b, c, n)
+    print('\n'.join(map(str, ans)))
+
+
+for _ in range(1):
+    solve()

c10cf0ff8f683f2bf68aefc9cdf4fffdbe0fff3d
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-13 17:30:00 +0530

    Add solution for problem `A - ABC Preparation` Link: https://atcoder.jp/contests/abc185/tasks/abc185_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc185/submissions/18721822

---
+++ atcoder/abc185/A.py
@@ -0,0 +1,22 @@
+# cook your dish here
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    l=li()
+    pr(min(l))
+for _ in range(1):
+    solve()

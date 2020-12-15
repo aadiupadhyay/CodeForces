c3e8c5322fe9eefaa8b15a8dacf65e5c479dad8b
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-15 22:48:00 +0530

    Add solution for problem `A - Favorite Sequence` Link: https://codeforces.com/contest/1462/problem/A Tags: implementation, two pointers, *800 Ref: https://codeforces.com/contest/1462/submission/101351670

---
+++ codeforces/1462/A.py
@@ -0,0 +1,53 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    l = li()
+    i = 0
+    j = n-1
+    while i < j:
+        print(l[i], l[j], end=' ')
+        i += 1
+        j -= 1
+    if n & 1:
+        print(l[n//2])
+    else:
+        print()
+
+
+for _ in range(inp()):
+    solve()

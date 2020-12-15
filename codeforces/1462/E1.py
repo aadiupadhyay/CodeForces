2ee14e05c7edbbf7d803a53f49ad2569dbb185ef
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-15 22:47:00 +0530

    Add solution for problem `E1 - Close Tuples (easy version)` Link: https://codeforces.com/contest/1462/problem/E1 Tags: binary search, combinatorics, math, sortings, two pointers, *1500 Ref: https://codeforces.com/contest/1462/submission/101351620

--- codeforces/1462/E1.py
+++ codeforces/1462/E1.py
@@ -21,9 +21,8 @@
 from sys import stdin, stdout
 from collections import *
 from math import gcd, floor, ceil, factorial
+from bisect import bisect_right
 def st(): return list(stdin.readline().strip())
-
-
 def li(): return list(map(int, stdin.readline().split()))
 def mp(): return map(int, stdin.readline().split())
 def inp(): return int(stdin.readline())
@@ -34,31 +33,17 @@ mod = 1000000007
 INF = float('inf')
 
 
-def ncr3(n):
-    if (3 > n or n < 0):
-        return 0
-    return (n*(n-1)*(n-2))//6
-
-
-def ncr2(n):
-    if (2 > n or n < 0):
-        return 0
-    return (n*(n-1))//2
-
-
 def solve():
     n = inp()
     l = li()
-    d = Counter(l)
     ans = 0
-    for i in d:
-        ans += ncr3(d[i])
-        ans += ncr2(d[i])*d[i+1]
-        ans += d[i]*ncr2(d[i+1])
-        ans += d[i]*d[i+1]*d[i+2]
-        ans += d[i]*ncr2(d[i+2])
-        ans += ncr2(d[i])*d[i+2]
-
+    l.sort()
+    for i, a in enumerate(l):
+        j = bisect_right(l, a+2)
+        if j-i-1 < 2:
+            continue
+        x = j-i-1
+        ans += (x*(x-1))//2
     pr(ans)
 
 

8c9151c8bfb8875e4550c1cec11d0eea9768ee7c
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-15 22:48:00 +0530

    Add solution for problem `C - Unique Number` Link: https://codeforces.com/contest/1462/problem/C Tags: brute force, greedy, math, *900 Ref: https://codeforces.com/contest/1462/submission/101351771

---
+++ codeforces/1462/C.py
@@ -0,0 +1,55 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+l = list(range(1, 51))
+
+
+def solve():
+    n = inp()
+    val = ''
+    for i in '987654321':
+        cur = val+i
+        x = sum(map(int, cur))
+        if x > n:
+            continue
+        elif x == n:
+            pr(cur[::-1])
+            return
+        val = cur
+    pr(-1)
+
+
+for _ in range(inp()):
+    solve()

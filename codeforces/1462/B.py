3188216f59d2baebd0eb04fc13dc94497ceeb402
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-15 22:48:00 +0530

    Add solution for problem `B - Last Year's Substring` Link: https://codeforces.com/contest/1462/problem/B Tags: dp, implementation, strings, *800 Ref: https://codeforces.com/contest/1462/submission/101351719

---
+++ codeforces/1462/B.py
@@ -0,0 +1,50 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    s = input()
+    for i in range(n+1):
+        for j in range(i, n+1):
+            a = s[:i]+s[j:]
+            if a == '2020':
+                pr('YES')
+                return
+    pr('NO')
+
+
+for _ in range(inp()):
+    solve()

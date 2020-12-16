a0c0f7f0a48e3e11392deed637a71ef6bfc3fa3a
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-16 23:32:00 +0530

    Add solution for problem `B - Fridge Lockers` Link: https://codeforces.com/contest/1255/problem/B Tags: graphs, implementation, *1100 Ref: https://codeforces.com/contest/1255/submission/101453397

---
+++ codeforces/1255/B.py
@@ -0,0 +1,53 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n, m = mp()
+    l = li()
+    if n == 2:
+        pr(-1)
+        return
+    if n != m:
+        pr(-1)
+        return
+    pr(2*sum(l))
+    for i in range(1, m):
+        print(i, i+1)
+    print(1, m)
+
+
+for _ in range(inp()):
+    solve()

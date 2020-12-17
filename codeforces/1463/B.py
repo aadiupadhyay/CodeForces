abbc6a0ca11ed524b4a9ea88c7f9163d33f191e8
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-17 22:43:00 +0530

    Add solution for problem `B - Find The Array` Link: https://codeforces.com/contest/1463/problem/B Tags: bitmasks, constructive algorithms, greedy, *1400 Ref: https://codeforces.com/contest/1463/submission/101583473

---
+++ codeforces/1463/B.py
@@ -0,0 +1,44 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    l = li()
+    for i in range(n):
+        print(pow(2, len(bin(l[i]))-3), end=' ')
+    print()
+
+
+for _ in range(inp()):
+    solve()

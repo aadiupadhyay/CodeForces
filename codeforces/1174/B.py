e8b23abca8075e096956359f5dbb17ab448d02f4
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-18 01:33:00 +0530

    Add solution for problem `B - Ehab Is an Odd Person` Link: https://codeforces.com/contest/1174/problem/B Tags: sortings, *1200 Ref: https://codeforces.com/contest/1174/submission/101593262

---
+++ codeforces/1174/B.py
@@ -0,0 +1,52 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    l = li()
+    even, odd = [], []
+    for i in range(n):
+        if l[i] & 1:
+            odd.append(i)
+        else:
+            even.append(i)
+    if even and odd:
+        l.sort()
+    print(*l)
+
+
+for _ in range(1):
+    solve()

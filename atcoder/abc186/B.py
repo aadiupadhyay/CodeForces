cd4080bba8bb522cbde03cd54749a698c685a8d8
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-19 17:33:00 +0530

    Add solution for problem `B - Blocks on Grid` Link: https://atcoder.jp/contests/abc186/tasks/abc186_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc186/submissions/18865300

---
+++ atcoder/abc186/B.py
@@ -0,0 +1,54 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n, m = mp()
+    d = defaultdict(int)
+    for i in range(n):
+        l = li()
+        for j in l:
+            d[j] += 1
+    a = min(d)
+    ans = 0
+
+    for i in d:
+        if i != a:
+            ans += (i-a)*d[i]
+    pr(ans)
+
+
+for _ in range(1):
+    solve()

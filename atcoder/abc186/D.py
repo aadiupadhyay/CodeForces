50cb077c4ed8d9aad1bb03a4111eba6a1dce95ab
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2020-12-19 17:59:00 +0530

    Add solution for problem `D - Sum of difference` Link: https://atcoder.jp/contests/abc186/tasks/abc186_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc186/submissions/18878125

---
+++ atcoder/abc186/D.py
@@ -0,0 +1,48 @@
+# Aaditya Upadhyay
+# ..............
+# ╭━┳━╭━╭━╮╮
+# ┃┈┈┈┣▅╋▅┫┃
+# ┃┈┃┈╰━╰━━━━━━╮
+# ╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣
+# ╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉
+# ╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤
+# ╲┃┈┈┈┈╭━┳━━━━╯
+# ╲┣━━━━━━┫
+# ……….
+# .……. /´¯/)………….(\¯`\
+# …………/….//……….. …\….\
+# ………/….//……………....\….\
+# …./´¯/…./´¯\……/¯ `\…..\¯`\
+# ././…/…/…./|_…|.\….\….\…\.\
+# (.(….(….(…./.)..)...(.\.).).)
+# .\…………….\/../…....\….\/…………/
+# ..\…………….. /……...\………………../
+# …..\…………… (………....)……………./
+from sys import stdin, stdout
+from collections import *
+from math import gcd, floor, ceil
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    l = li()
+    l.sort()
+    ans = 0
+    for i in range(n-1, -1, -1):
+        ans += i*l[i] - (n-i-1)*l[i]
+    pr(ans)
+
+
+for _ in range(1):
+    solve()

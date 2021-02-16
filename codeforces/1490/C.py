a3ed07c69f40b76c6423c5cd791a6a0bc2400bdf
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-02-16 22:44:00 +0530

    Add solution for problem `C - Sum of Cubes` Link: https://codeforces.com/contest/1490/problem/C Tags: binary search, brute force, brute force, math, *1100 Ref: https://codeforces.com/contest/1490/submission/107624451

--- codeforces/1490/C.py
+++ codeforces/1490/C.py
@@ -1,35 +1,34 @@
-
 from sys import stdin, stdout
 from collections import *
 from math import gcd, floor, ceil
 def st(): return list(stdin.readline().strip())
-
-
+ 
+ 
 def li(): return list(map(int, stdin.readline().split()))
 def mp(): return map(int, stdin.readline().split())
 def inp(): return int(stdin.readline())
 def pr(n): return stdout.write(str(n)+"\n")
-
-
+ 
+ 
 mod = 1000000007
 INF = float('inf')
-
+ 
 
 def solve():
     n=inp()
-    x=1
-    y= int(n**(1/3))
-
-    while x<=y:
-        a,b = x*x*x, y*y*y
-        if a+b ==n:
-            pr('YES')
-            return 
-        elif a+b >n:
-            y-=1
+    a= int(pow(n,1/3))
+    b=1
+    c=0
+    while a>=b:
+        x= a*a*a + b*b*b
+        if x<n:
+            b+=1
+        elif x>n:
+            a-=1
         else:
-            x+=1
-    pr('NO')
-
+            c=1
+            break
+    
+    pr('NO' if c==0 else 'YES')
 for _ in range(inp()):
     solve()

0a7a6a4165f607c1ac558b6ed8de47d86f46b1fb
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-02-20 21:49:00 +0530

    Add solution for problem `A - Star` Link: https://atcoder.jp/contests/abc192/tasks/abc192_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc192/submissions/20353165

---
+++ atcoder/abc192/A.py
@@ -0,0 +1,23 @@
+
+from sys import stdin, stdout
+from collections import *
+def st(): return list(stdin.readline().strip())
+
+
+def li(): return list(map(int, stdin.readline().split()))
+def mp(): return map(int, stdin.readline().split())
+def inp(): return int(stdin.readline())
+def pr(n): return stdout.write(str(n)+"\n")
+
+
+mod = 1000000007
+INF = float('inf')
+
+
+def solve():
+    n = inp()
+    pr(100 - n % 100)
+
+
+for _ in range(1):
+    solve()

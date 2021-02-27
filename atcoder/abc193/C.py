1841e792a1d8c13b0de583294b819c6066f935c8
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-02-28 00:47:00 +0530

    Add solution for problem `C - Unexpressed` Link: https://atcoder.jp/contests/abc193/tasks/abc193_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc193/submissions/20562241

---
+++ atcoder/abc193/C.py
@@ -0,0 +1,35 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    n = inp()
+    x = set()
+    cur = 2
+    while cur*cur <= n:
+        a = cur * cur
+        while a <= n:
+            x.add(a)
+            a *= cur
+        cur += 1
+    pr(n-len(x))
+
+
+for _ in range(1):
+    solve()

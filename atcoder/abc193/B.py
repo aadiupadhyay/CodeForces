7e01214f41973f28d6d2bdfb527587cab2a44859
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-02-28 00:53:00 +0530

    Add solution for problem `B - Play Snuke` Link: https://atcoder.jp/contests/abc193/tasks/abc193_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc193/submissions/20562274

---
+++ atcoder/abc193/B.py
@@ -0,0 +1,32 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    ans = INF
+    n = inp()
+    for i in range(n):
+        a, b, c = mp()
+        if c > a and ans > b:
+            ans = b
+    pr(-1 if ans == INF else ans)
+
+
+for _ in range(1):
+    solve()

a14369da6c48dfa286268c73afea3cc469ebaece
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-03-11 16:01:00 +0530

    Add solution for problem `B - Fenwick Tree` Link: https://atcoder.jp/contests/practice2/tasks/practice2_b Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/practice2/submissions/20828888

--- atcoder/practice2/B.py
+++ atcoder/practice2/B.py
@@ -1,49 +1,30 @@
-# aadiupadhyay
-import os.path
-from math import gcd, floor, ceil
-from collections import *
-import sys
-mod = 1000000007
-INF = float('inf')
-def st(): return list(sys.stdin.readline().strip())
-def li(): return list(map(int, sys.stdin.readline().split()))
-def mp(): return map(int, sys.stdin.readline().split())
-def inp(): return int(sys.stdin.readline())
-def pr(n): return sys.stdout.write(str(n)+"\n")
-def prl(n): return sys.stdout.write(str(n)+" ")
-
-
-if os.path.exists('input.txt'):
-    sys.stdin = open('input.txt', 'r')
-    sys.stdout = open('output.txt', 'w')
-
-
-def update(ind, x):
-    while ind <= n:
-        fenwick[ind] += x
-        ind += ind & -ind
-
-
-def sum(ind):
-    s = 0
-    while ind > 0:
-        s += fenwick[ind]
-        ind -= ind & -ind
-    return s
-
-
-n, q = mp()
-l = [0]+li()
-fenwick = [0 for i in range(n+10)]
-for i in range(1, n+1):
-    update(i, l[i])
+def presum(indx,fen,n):
+  s=0
+  while indx>0:
+    s=s+fen[indx]
+    indx=indx-(indx&(-indx))
+  return s
+def query(l,r,fen,n):
+  return presum(r,fen,n)-presum(l,fen,n)
+def update(indx,delta,fen,n):
+  while indx<n:
+    fen[indx]+=delta
+    indx=indx+(indx&(-indx))
+  return fen
+def buildtree(l,n):
+  fen=[0]*n 
+  for i in range(1,n):
+    fen=update(i,l[i],fen,n)
+  return fen
+n,q=map(int,input().split())
+l=list(map(int,input().split()))
+l=[0]+l
+n=n+1
+fen=buildtree(l,n)
 for i in range(q):
-    a, b, c = mp()
-    if a == 0:
-        b += 1
-        update(b, c)
-    else:
-        # make 1 base index
-        b += 1
-        c += 1
-        print(sum(c-1)-sum(b-1))
+  a=list(map(int,input().split()))
+  if a[0]==0:
+    update(a[1]+1,a[2],fen,n)
+  else:
+    print(query(a[1],a[2],fen,n))
+    		

bb98f9816cd9c53fe00328257e0d5b9a09bcb255
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-03-21 01:18:00 +0530

    Add solution for problem `C1 - k-LCM (easy version)` Link: https://codeforces.com/contest/1497/problem/C1 Tags: constructive algorithms, math, *1200 Ref: https://codeforces.com/contest/1497/submission/110569952

---
+++ codeforces/1497/C1.py
@@ -0,0 +1,33 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    n, k = mp()
+    if n & 1:
+        print(n//2, n//2, 1)
+        return
+    if n % 4 == 0:
+        print(n//4, n//4, n//2)
+        return
+    print(2, n//2-1, n//2-1)
+
+
+for _ in range(inp()):
+    solve()

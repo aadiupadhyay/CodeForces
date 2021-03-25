4159da6305d3d75697dfdb18fb60d0b7fd5ebedd
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-03-25 22:21:00 +0530

    Add solution for problem `C - Double-ended Strings` Link: https://codeforces.com/contest/1506/problem/C Tags: brute force, dp, hashing, strings Ref: https://codeforces.com/contest/1506/submission/111063846

---
+++ codeforces/1506/C.py
@@ -0,0 +1,36 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    a = input()
+    b = input()
+    n, m = len(a), len(b)
+    dp = [[0 for i in range(m+1)] for j in range(n+1)]
+    ans = 0
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = 1+dp[i-1][j-1]
+                ans = max(ans, dp[i][j])
+    pr(n+m-2*ans)
+
+
+for _ in range(inp()):
+    solve()

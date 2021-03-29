3021c3654e49ef17b1122c66a4f16ca359688328
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-03-29 23:25:00 +0530

    Add solution for problem `A - GCD Sum` Link: https://codeforces.com/contest/1498/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1498/submission/111410571

---
+++ codeforces/1498/A.py
@@ -0,0 +1,29 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    n = inp()
+    while gcd(n, sum(map(int, str(n)))) == 1:
+        n += 1
+    pr(n)
+
+
+for _ in range(inp()):
+    solve()

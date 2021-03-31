daca8418cc54b089f27e310b3062d6240e4f14fe
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-03-31 15:07:00 +0530

    Add solution for problem `B - Ilya and Queries` Link: https://codeforces.com/contest/313/problem/B Tags: dp, implementation, *1100 Ref: https://codeforces.com/contest/313/submission/111543273

--- codeforces/313/B.py
+++ codeforces/313/B.py
@@ -1,81 +1,33 @@
-from sys  import stdin,stdout
-
-import bisect
-
-def st():
-    return list(stdin.readline())
-
-def inp():
-    return int(stdin.readline())
-
-def li():
-    return list(map(int,stdin.readline().split()))
-
-def mp():
-    return map(int,stdin.readline().split())
-
-def pr(n):
-    stdout.write(str(n)+"\n")
-
-def soe(limit):
-    l=[1]*(limit+1)
-    prime=[]
-    for i in range(2,limit+1):
-        if l[i]:
-            for j in range(i*i,limit+1,i):
-                l[j]=0
-
-    for i in range(2,limit+1):
-        if l[i]:
-            prime.append(i)
-    return prime
-
-def segsoe(low,high):
-    limit=int(high**0.5)+1
-    prime=soe(limit)
-    n=high-low+1
-    l=[0]*(n+1)
-    for i in range(len(prime)):
-        lowlimit=(low//prime[i])*prime[i]
-        if lowlimit<low:
-            lowlimit+=prime[i]
-        if lowlimit==prime[i]:
-            lowlimit+=prime[i]
-        for j in range(lowlimit,high+1,prime[i]):
-            l[j-low]=1
-    for i in range(low,high+1):
-        if not l[i-low]:
-            if i!=1:
-                print(i)
-
-def power(a,n):
-    r=1
-    while n:
-        if n&1:
-            r=(r*a)
-            n=n-1
-        else:
-            a=(a*a)
-            n=n>>1
-    return r
-    
-def solve():
-    a,b=mp()
-    print(l[b-1]-l[a-1])
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+from bisect import *
+import sys
+from random import *
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
 
 
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
 
-
-
-s=st()
-l=[0]
-for i in range(1,len(s)):
-    if s[i]==s[i-1]:
-        l.append(l[i-1]+1)
-    else:
-        l.append(l[i-1])
-
-    
-for _ in range(inp()):
-    solve()
-    
+s = st()
+n = len(s)
+m = inp()
+l = [li() for i in range(m)]
+ans = [0 for i in range(n+1)]
+for i in range(1, n):
+    ans[i] = ans[i-1]
+    if s[i-1] == s[i]:
+        ans[i] += 1
+ans[-1] = ans[-2]
+for i in l:
+    print(ans[i[1]-1]-ans[i[0]-1])

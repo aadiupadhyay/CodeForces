ea41506b6746c71db1f14199571dc25af3d3afa6
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-05 00:21:00 +0530

    Add solution for problem `A - Filling Shapes` Link: https://codeforces.com/contest/1182/problem/A Tags: dp, math, *1000 Ref: https://codeforces.com/contest/1182/submission/112027744

---
+++ codeforces/1182/A.py
@@ -0,0 +1,25 @@
+# aadiupadhyay
+import sys
+from collections import *
+import os.path
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    n = inp()
+    pr(pow(2, n//2) if n % 2 == 0 else 0)
+
+
+solve()

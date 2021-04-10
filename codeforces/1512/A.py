0ec03c22c995eee0136f12b44c162720404146de
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-10 22:36:00 +0530

    Add solution for problem `A - Spy Detected!` Link: https://codeforces.com/contest/1512/problem/A Tags: brute force, implementation Ref: https://codeforces.com/contest/1512/submission/112564896

---
+++ codeforces/1512/A.py
@@ -0,0 +1,32 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    n = inp()
+    l = li()
+    d = {l[i]: i+1 for i in range(n)}
+    for i in d:
+        if l.count(i) == 1:
+            print(d[i])
+            return
+
+
+for _ in range(inp()):
+    solve()

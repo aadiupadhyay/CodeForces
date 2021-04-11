b0342b4aeadf60462da897082251e9e850055533
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-12 00:11:00 +0530

    Add solution for problem `C - Add One` Link: https://codeforces.com/contest/1513/problem/C Tags: dp, matrices, *1600 Ref: https://codeforces.com/contest/1513/submission/112720326

---
+++ codeforces/1513/C.py
@@ -0,0 +1,40 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+maxN = 200000 + 5
+dp = [[0 for i in range(10)] for j in range(maxN+1)]
+for i in range(10):
+    dp[0][i] = 1
+
+for i in range(1, maxN+1):
+    for j in range(9):
+        dp[i][j] = dp[i-1][j+1]
+    dp[i][9] = (dp[i-1][0] + dp[i-1][1]) % mod
+
+
+def solve():
+    n, m = mp()
+    ans = 0
+    for i in str(n):
+        a = int(i)
+        ans = (ans + dp[m][a]) % mod
+    pr(ans)
+
+
+for _ in range(inp()):
+    solve()

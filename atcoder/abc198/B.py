c95967eccbe2b92883cbd725472c5738f4d179e0
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-12 02:11:00 +0530

    Add solution for problem `B - Palindrome with leading zeros` Link: https://atcoder.jp/contests/abc198/tasks/abc198_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc198/submissions/21703072

---
+++ atcoder/abc198/B.py
@@ -0,0 +1,28 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+n = inp()
+s = str(n)
+i = len(s)-1
+while i >= 0 and s[i] == '0':
+    i -= 1
+if s[:i+1] == s[:i+1][::-1]:
+    pr('Yes')
+else:
+    pr('No')

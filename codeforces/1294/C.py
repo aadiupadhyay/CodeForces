a7d35fc175feb660e0eb938e564230005594ab02
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-12 15:12:00 +0530

    Add solution for problem `C - Product of Three Numbers` Link: https://codeforces.com/contest/1294/problem/C Tags: greedy, math, number theory, *1300 Ref: https://codeforces.com/contest/1294/submission/112763487

--- codeforces/1294/C.py
+++ codeforces/1294/C.py
@@ -1,28 +1,65 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
 
-def fact(n):
-    i=2
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0 and i not in l:
-            return i
-    return 0
-for _ in range(int(input())):
-    l=[]
-    n=int(input())
-    a=fact(n)
-    l.append(a)
-    if a!=0:
-        b=fact(n/a)
-    else:
-        print("NO")
-        continue
-    if b!=0:
-        z=(n//(a*b))
-    else:
-        print("NO")
-        continue
-    if z!=a and z!=b and z!=1:
-        print("YES")
-        print(a,b,z)
-    else:
-        print("NO")
-    
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+
+    def factor(n, x):
+
+        if isprime(n):
+            return False
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                if i != n//i:
+                    if i != x and n//i != x:
+                        print('YES')
+                        print(i, n//i, x)
+                        return True
+            i += 1
+        return False
+
+    def isprime(n):
+        if n <= 4:
+            return n == 2 or n == 3
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        for i in range(5, int(n**0.5)+1, 6):
+            if n % i == 0 or n % (i+2) == 0:
+                return False
+        return True
+    n = inp()
+    if n == 1 or isprime(n):
+        pr('NO')
+        return
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            if n//i != i:
+                a, b = n//i, i
+                if factor(a, b):
+                    return
+                if factor(b, a):
+                    return
+        i += 1
+    pr('NO')
+
+
+for _ in range(inp()):
+    solve()

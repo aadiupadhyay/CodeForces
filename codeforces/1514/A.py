cde7b87601e9c6158fff0a589fa5d0bc515455d2
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-19 23:49:00 +0530

    Add solution for problem `A - Perfectly Imperfect Array` Link: https://codeforces.com/contest/1514/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1514/submission/113548939

---
+++ codeforces/1514/A.py
@@ -0,0 +1,33 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    n = inp()
+    l = li()
+    for i in l:
+        a = i**0.5
+        if ceil(a) != floor(a):
+            pr('YES')
+            return
+    pr('NO')
+
+
+for _ in range(inp()):
+    solve()

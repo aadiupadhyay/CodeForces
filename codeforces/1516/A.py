3da93c406f6e52739368b72627977c0c7eaca7bb
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-21 21:42:00 +0530

    Add solution for problem `A - Tit for Tat` Link: https://codeforces.com/contest/1516/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1516/submission/113792810

---
+++ codeforces/1516/A.py
@@ -0,0 +1,35 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def solve():
+    n, k = mp()
+    l = li()
+    i = 0
+    j = n-1
+    for i in range(n-1):
+        a = min(l[i], k)
+        l[i] -= a
+        l[-1] += a
+        k -= a
+    print(*l)
+
+
+for _ in range(inp()):
+    solve()

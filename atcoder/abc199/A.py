1fecae0a2b7996b7c1a06aacc03a9deb2af5d728
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-04-25 00:42:00 +0530

    Add solution for problem `A - Square Inequality` Link: https://atcoder.jp/contests/abc199/tasks/abc199_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc199/submissions/22046312

--- atcoder/abc199/A.py
+++ atcoder/abc199/A.py
@@ -1,5 +1,23 @@
-a,b,c=map(int,input().split())
-if a**2+b**2<c**2:
-  print("Yes")
-else:
-  print("No")
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+a, b, c = mp()
+x = a*a + b*b
+pr('Yes' if x < c*c else 'No')

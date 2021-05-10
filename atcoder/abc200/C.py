a7d07b8ca2b3c2c3ce123c3cff97c63da10bc383
Aaditya Upadhyay <upadhyay.aaditya2001@gmail.com>
2021-05-11 00:17:00 +0530

    Add solution for problem `C - Ringo's Favorite Numbers 2` Link: https://atcoder.jp/contests/abc200/tasks/abc200_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc200/submissions/22508548

---
+++ atcoder/abc200/C.py
@@ -0,0 +1,27 @@
+# aadiupadhyay
+import os.path
+from math import gcd, floor, ceil
+from collections import *
+import sys
+mod = 1000000007
+INF = float('inf')
+def st(): return list(sys.stdin.readline().strip())
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def pr(n): return sys.stdout.write(str(n)+"\n")
+def prl(n): return sys.stdout.write(str(n)+" ")
+
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+n = inp()
+l = li()
+ans = 0
+d = defaultdict(int)
+for i in l:
+    ans += d[i % 200]
+    d[i % 200] += 1
+pr(ans)
